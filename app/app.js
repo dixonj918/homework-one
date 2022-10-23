a2d897d2538093554c21160b46d0d661ba14899e
dixonj918 <dixon.james@outlook.com>
2022-10-23 16:36:18 -0400

    Updated model.js

--- app/app.js
+++ app/app.js
@@ -1,21 +1,3 @@
-function changeRoute(){
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
-    console.log(hashTag);
-
-    if(pageID !="") {
-    $.get(`pages/${pageID}/${pageID}.html`, function(data){
-        console.log("data" + data);
-        $("#app").html(data);
-    });
-  }else {
-    $.get(`pages/home/home.html`, function(data){
-        console.log("data" + data);
-        $("#app").html(data);
-    });
-  }
-}
-
 function initURLListerner(){
     $(window).on("hashchange", changeRoute);
     changeRoute();
